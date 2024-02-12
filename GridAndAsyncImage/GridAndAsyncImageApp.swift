9bf955eb3bef30ab19f20d469925ea9dce035bff
naelafauzul <naelafauzulm@gmail.com>
2024-02-12 13:40:31 +0700

    Initial Commit

---
+++ GridAndAsyncImage/GridAndAsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridAndAsyncImageApp.swift
+//  GridAndAsyncImage
+//
+//  Created by Naela Fauzul Muna on 12/02/24.
+//
+
+import SwiftUI
+
+@main
+struct GridAndAsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
